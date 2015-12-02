48a872d7e9be275819430373157cdcee321b6dcd
Mohthorn <mohthorn@outlook.com>
2015-12-02 17:02:36 +0800

    Artificial inteligence

---
+++ checker/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace checker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.inputButton = new System.Windows.Forms.Button();
+            this.inputText = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // inputButton
+            // 
+            this.inputButton.Location = new System.Drawing.Point(721, 160);
+            this.inputButton.Name = "inputButton";
+            this.inputButton.Size = new System.Drawing.Size(106, 65);
+            this.inputButton.TabIndex = 0;
+            this.inputButton.Text = "确认";
+            this.inputButton.UseVisualStyleBackColor = true;
+            this.inputButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // inputText
+            // 
+            this.inputText.Location = new System.Drawing.Point(689, 96);
+            this.inputText.Name = "inputText";
+            this.inputText.Size = new System.Drawing.Size(174, 25);
+            this.inputText.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1008, 526);
+            this.Controls.Add(this.inputText);
+            this.Controls.Add(this.inputButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button inputButton;
+        private System.Windows.Forms.TextBox inputText;
+    }
+}
+
---
+++ checker/Form1.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace checker
+{
+    public partial class Form1 : Form
+    {
+        public int[] tags = new int[64];
+        
+        public Form1()
+        {
+            InitializeComponent();
+            initialTags();
+            AddPictureBox();
+            
+        }
+        private int AiGo()
+        {
+            int n = 0;
+            Random r = new Random();
+            int iUp = 64;
+            int iDown = -1;
+            n = r.Next(iDown, iUp);
+            while (tags[n] != 0)
+            {
+                r = new Random();
+                iUp = 64;
+                iDown = -1;
+                n = r.Next(iDown, iUp);
+            }
+            return n;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int n;
+            n = Convert.ToInt32(inputText.Text);
+            if (n >= 0 && n <= 63)
+            {
+                if (tags[n] == 0)
+                {
+                    tags[n] = 1;
+                    PictureBox Pb = new PictureBox();
+                    int i = n / 8;
+                    int j = n % 8;
+
+
+
+
+                    Pb.Height = 40;//每个格子高度 宽度均为70
+                    Pb.Width = 40;//
+                    Pb.Top = i * 40 + 40;//起始坐标
+                    Pb.Left = 40 + j * 40;
+                    Pb.BackColor = Color.Black;
+                    this.Controls.Add(Pb);
+                    Pb.BringToFront();
+
+                    int nAi;
+                    nAi=AiGo();
+                    tags[nAi] = 1;
+                    Pb = new PictureBox();
+                    i = nAi / 8;
+                    j = nAi % 8;
+
+
+
+
+                    Pb.Height = 40;//每个格子高度 宽度均为70
+                    Pb.Width = 40;//
+                    Pb.Top = i * 40 + 40;//起始坐标
+                    Pb.Left = 40 + j * 40;
+                    Pb.BackColor = Color.Red;
+                    this.Controls.Add(Pb);
+                    Pb.BringToFront();
+
+                }
+                else
+                {
+                    MessageBox.Show("hehe");
+                }
+            }
+            else
+            {
+                MessageBox.Show("hehe");
+            }
+
+        }
+        public void initialTags()
+        {
+            for (int i = 0; i < 64; i++)
+                tags[i] = 0;
+        }
+        //PictureBox[] Pb = new PictureBox[64];//构造棋盘用
+        Label[] Lb = new Label[64];
+
+        
+
+        private void AddPictureBox()
+        {
+            int N = 0;
+            for (int i = 0; i < 8; i++)//8
+            {
+                for (int j = 0; j < 8; j++)//8
+                {
+                    N = j + i * 8;
+                    //Pb[N] = new PictureBox();//从第一列由上而下依次编号0到63,共64个格子
+                    //Pb[N].Height = 40;//每个格子高度 宽度均为70
+                    //Pb[N].Width = 40;//
+                    //Pb[N].Top = i * 40 + 40;//起始坐标
+                    //Pb[N].Left = 40+j * 40;//按行每次在窗体放置8个控件，然后在i+1；
+                    Lb[N] = new Label();//从第一列由上而下依次编号0到63,共64个格子
+                    Lb[N].Height = 40;//每个格子高度 宽度均为70
+                    Lb[N].Width = 40;//
+                    Lb[N].Top = i * 40 + 40;//起始坐标
+                    Lb[N].Left = 40 + j * 40;//按行每次在窗体放置8个控件，然后在i+1；
+
+                    if (i % 2 == j % 2)
+                    {
+                        //Pb[N].BackColor = Color.Green;//灰白间隔效果
+                        Lb[N].Text = N.ToString();
+                        Lb[N].BackColor = Color.Green;
+                    }
+                    else
+                    {
+                        //Pb[N].BackColor = Color.White;
+                        Lb[N].Text = N.ToString();
+                        Lb[N].BackColor = Color.White;
+                    }
+                    //this.Controls.Add(Pb[N]);//没有这个语句，控件是无法添加到窗体上的！
+                    this.Controls.Add(Lb[N]);
+                    Lb[N].BringToFront();
+                }
+
+            }
+
+
+        }
+    }
+}
