5ce23e4afe3921fdc43bfaefd1d3c5178208ea25
Mohthorn <mohthorn@outlook.com>
2015-12-21 18:55:13 +0800

    opencv库函数GaussianBlur边缘加强

--- imageProcessing1/imageProcessing1/source.cpp
+++ imageProcessing1/imageProcessing1/source.cpp
@@ -1,16 +1,23 @@
 #include<iostream>
 #include <opencv2/core/core.hpp>   
 #include <opencv2/highgui/highgui.hpp>
+#include "cv.h"
+#include "highgui.h"
+#include "opencv2/imgproc/imgproc.hpp"
+#define PI 3.14
 using namespace std;
 using namespace cv;
 
 
 int main()
 {
+	int m[100][100];
+	
 	Mat img = imread("imgs/Art1.bmp");
 	Mat dimg = imread("imgs/Art1_depth.bmp");
-	Mat omg = img;
-	Mat D = img;
+	Mat dimg2= imread("imgs/Art1_depth.bmp");
+	Mat omg = dimg;
+	Mat DD = dimg2;
 	if (img.empty())
 	{
 		cout << "error";
@@ -23,22 +30,40 @@ int main()
 
 	imshow("Art1", img);
 	imshow("Art1_D", dimg);
-	int diag = int(sqrt(img.rows*img.rows + img.cols * img.cols));
-	
-	cout << img.rows << endl << img.cols <<endl<<diag<< endl;
-	
+	//int diag = int(sqrt(img.rows*img.rows + img.cols * img.cols));
+	//
+	//cout << img.rows << endl << img.cols <<endl<<diag<< endl;
+//¸ßË¹ÂË²¨	
+	//int k = 5;
+	//cin >> k;
+	//for (int i = 0; i < 2 * k + 1; i++)
+	//{
+	//	for (int j = 0; j < 2 * k + 1; j++)
+	//	{
+	//		int dis = (i - k)*(i - k) + (j - k)*(j - k);
+	//		m[i][j] = 0;
+	//	}
+	//}
+	//for (int i = 0; i < 2 * k + 1; i++)
+	//{
+	//	for (int j = 0; j < 2 * k + 1; j++)
+	//	{
+	//		cout<<m[i][j]<<" ";
+	//	}
+	//	cout << endl;
+	//}
+	GaussianBlur(dimg, omg, Size(7, 7), 0, 0);
+	imshow("gauss", omg);
 
-	for (int i = 0; i < img.rows; i++)
-		for (int j = 0; j < img.cols; j++)
-		{
-			Vec3b pixel;
-			pixel[0] = 0;
-			pixel[1] = 0;
-			pixel[2] = 0;
-			img.at<Vec3b>(i, j) = pixel;
-		}
+	imshow("DD", DD);
+
+	DD = omg - dimg2;
+	imshow("DD", DD);
+	imshow("Art21", img);
+	img -= (3*DD);
+
 	imshow("Art2", img);
-	//cout << "img=" << endl << dimg << endl;
+
 	waitKey();
 
 	return 0;
