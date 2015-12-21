22ec8a6a829ed50a38bf938e893ee6d17f269886
Mohthorn <mohthorn@outlook.com>
2015-12-21 12:19:08 +0800

    nothing important

--- imageProcessing1/imageProcessing1/source.cpp
+++ imageProcessing1/imageProcessing1/source.cpp
@@ -3,15 +3,42 @@
 #include <opencv2/highgui/highgui.hpp>
 using namespace std;
 using namespace cv;
+
+
 int main()
 {
-	Mat img = imread("Art1.bmp");
+	Mat img = imread("imgs/Art1.bmp");
+	Mat dimg = imread("imgs/Art1_depth.bmp");
+	Mat omg = img;
+	Mat D = img;
 	if (img.empty())
 	{
 		cout << "error";
 		return -1;
 	}
+	if (dimg.empty())
+	{
+		cout << endl << "derror";
+	}
+
 	imshow("Art1", img);
+	imshow("Art1_D", dimg);
+	int diag = int(sqrt(img.rows*img.rows + img.cols * img.cols));
+	
+	cout << img.rows << endl << img.cols <<endl<<diag<< endl;
+	
+
+	for (int i = 0; i < img.rows; i++)
+		for (int j = 0; j < img.cols; j++)
+		{
+			Vec3b pixel;
+			pixel[0] = 0;
+			pixel[1] = 0;
+			pixel[2] = 0;
+			img.at<Vec3b>(i, j) = pixel;
+		}
+	imshow("Art2", img);
+	//cout << "img=" << endl << dimg << endl;
 	waitKey();
 
 	return 0;
