12fa1ee6dc370dd7d4c0b3aa31dd1a1bd7f87ee6
Mohthorn <mohthorn@outlook.com>
2015-12-21 19:44:11 +0800

    全部图像边缘锐化

--- imageProcessing1/imageProcessing1/source.cpp
+++ imageProcessing1/imageProcessing1/source.cpp
@@ -11,59 +11,63 @@ using namespace cv;
 
 int main()
 {
+	String pics[8] = { "Art1","Bowling1","cones","Dolls","Flowerpots","Laundry1","Moebius1","teddy" };
 	int m[100][100];
-	
-	Mat img = imread("imgs/Art1.bmp");
-	Mat dimg = imread("imgs/Art1_depth.bmp");
-	Mat dimg2= imread("imgs/Art1_depth.bmp");
-	Mat omg = dimg;
-	Mat DD = dimg2;
-	if (img.empty())
+	for (int ii = 0; ii < 8; ii++)
 	{
-		cout << "error";
-		return -1;
+		String s1 = "imgs/" + pics[ii] + ".bmp";
+		String s2= "imgs/" + pics[ii] + "_depth.bmp";
+		Mat img = imread(s1);
+		Mat dimg = imread(s2);
+		Mat dimg2 = imread(s2);
+		Mat omg = dimg;
+		Mat DD = dimg2;
+		if (img.empty())
+		{
+			cout << "error";
+			return -1;
+		}
+		if (dimg.empty())
+		{
+			cout << endl << "derror";
+		}
+
+		//imshow("Art1", img);
+		//imshow("Art1_D", dimg);
+		//int diag = int(sqrt(img.rows*img.rows + img.cols * img.cols));
+		//
+		//cout << img.rows << endl << img.cols <<endl<<diag<< endl;
+	//高斯滤波	
+		//int k = 5;
+		//cin >> k;
+		//for (int i = 0; i < 2 * k + 1; i++)
+		//{
+		//	for (int j = 0; j < 2 * k + 1; j++)
+		//	{
+		//		int dis = (i - k)*(i - k) + (j - k)*(j - k);
+		//		m[i][j] = 0;
+		//	}
+		//}
+		//for (int i = 0; i < 2 * k + 1; i++)
+		//{
+		//	for (int j = 0; j < 2 * k + 1; j++)
+		//	{
+		//		cout<<m[i][j]<<" ";
+		//	}
+		//	cout << endl;
+		//}
+		GaussianBlur(dimg, omg, Size(31, 31), 0, 0);
+		//imshow("gauss", omg);
+
+		//imshow("DD", DD);
+
+		DD = omg - dimg2;
+		//imshow("DD", DD);
+		//imshow("Art21", img);
+		img -= (3 * DD);
+		String s3= "imgs/" + pics[ii] + "_out.bmp";
+		imwrite(s3, img);
 	}
-	if (dimg.empty())
-	{
-		cout << endl << "derror";
-	}
-
-	imshow("Art1", img);
-	imshow("Art1_D", dimg);
-	//int diag = int(sqrt(img.rows*img.rows + img.cols * img.cols));
-	//
-	//cout << img.rows << endl << img.cols <<endl<<diag<< endl;
-//高斯滤波	
-	//int k = 5;
-	//cin >> k;
-	//for (int i = 0; i < 2 * k + 1; i++)
-	//{
-	//	for (int j = 0; j < 2 * k + 1; j++)
-	//	{
-	//		int dis = (i - k)*(i - k) + (j - k)*(j - k);
-	//		m[i][j] = 0;
-	//	}
-	//}
-	//for (int i = 0; i < 2 * k + 1; i++)
-	//{
-	//	for (int j = 0; j < 2 * k + 1; j++)
-	//	{
-	//		cout<<m[i][j]<<" ";
-	//	}
-	//	cout << endl;
-	//}
-	GaussianBlur(dimg, omg, Size(7, 7), 0, 0);
-	imshow("gauss", omg);
-
-	imshow("DD", DD);
-
-	DD = omg - dimg2;
-	imshow("DD", DD);
-	imshow("Art21", img);
-	img -= (3*DD);
-
-	imshow("Art2", img);
-
 	waitKey();
 
 	return 0;
